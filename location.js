ccb336c251094e9597cf21fdcd7002955b43a97a
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-01-20 03:41:11 +0530

    Update location.js

--- location.js
+++ location.js
@@ -1,42 +1,47 @@
-function detectlocation(){
-    if(navigator.geolocation){
+// Checking for browser support and if browser support geolocation api, call getCurrentPosition method.
+function detectlocation() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showLocation, showError);
     }
-    else{
+    else {
         alert("Your browser not support geolocation api");
     }
 }
 
-function showLocation(pos){
+// A success callback function to get the location of user.
+function showLocation(pos) {
     latt = pos.coords.latitude;
     long = pos.coords.longitude;
     var lattlong = new google.maps.LatLng(latt, long);
     var options = {
         center: lattlong,
-        zoom:15,
+        zoom: 15,
         mapTypeControl: true,
-        navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL}
+        navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }
     }
-    var coordinates = "Your coordinates are - " + "latitude: " + latt + ",   longitude: " + long ;
+    var coordinates = "Your coordinates are - " + "latitude: " + latt + ",   longitude: " + long;
     document.getElementById("position").innerHTML = coordinates;
     var maps = new google.maps.Map(document.getElementById("location"), options);
-    var markers = new google.maps.Marker({position:lattlong, map:maps, title:"your position"});
+    // To mark user location on map google maps provide marker.
+    var markers = new google.maps.Marker({ position: lattlong, map: maps, title: "your position" });
 }
- 
-function showError(error) {  
-              switch(error.code){  
-              case error.PERMISSION_DENIED:  
-              alert("User denied the request.");  
-              break;  
-             case error.POSITION_UNAVAILABLE:  
-             alert("USer location information is not available.");  
-            break;  
-            case error.TIMEOUT:  
-            alert("The request to get user location timed out.");  
-            break;  
-           case error.UNKNOWN_ERROR:  
-            alert("An unknown error occurred.");  
-            break;  
-           }  
+
+// An error callback function 
+function showError(error) {
+    switch (error.code) {
+    case error.PERMISSION_DENIED:
+        alert("User denied the request.");
+        break;
+    case error.POSITION_UNAVAILABLE:
+        alert("USer location information is not available.");
+        break;
+    case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+    case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
 }
-       
+
+
